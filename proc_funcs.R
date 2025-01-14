e718fa1d0ff9c67f335361c8807c597f8d00c351
Allan Sims <asims@emu.ee>
2025-01-14 11:51:05 +0200

    Uuendus

--- proc_funcs.R
+++ proc_funcs.R
@@ -1,7 +1,7 @@
 options(OutDec = ",")
 
 print_termin <- function(x) {
-  paste0('\\index{',x,'}','**',x,'**')
+  paste0('[',x,']', "(https://sonaveeb.ee/search/unif/dlall/aso/",gsub(" ", "%20", tolower(x)),"/1/est)") 
 }
 
 
