1a52e755fca8484968d80ad56c1678a3d3759804
Allan Sims <asims@emu.ee>
2025-02-10 06:38:21 +0200

    Uuendus

--- proc_funcs.R
+++ proc_funcs.R
@@ -1,7 +1,27 @@
-require(mgcv)
 knitr::opts_chunk$set(
-  comment = '', fig.width = 8, fig.height = 6, out.width = "80%", size = "small"
+  comment = '', fig.width = 7, fig.height = 4, out.width = "80%", size = "small", fig.pos = "H"
 )
+
+
+knitr::knit_engines$set(naidis = function(options) {
+  excel_env_start <- '\n\\begin{naidis}\n'
+  excel_env_end <- '\n\\end{naidis}\n'
+  
+  excel_code <- paste(excel_env_start,
+                      paste0(options$code, "\n"),
+                      excel_env_end, sep="")
+  knitr::asis_output(excel_code)
+})
+
+knitr::knit_engines$set(excel = function(options) {
+  excel_env_start <- '\n\\begin{naideExcel}\n'
+  excel_env_end <- '\n\\end{naideExcel}\n'
+  
+  excel_code <- paste(excel_env_start,
+                      paste0(options$code, "\n"),
+                      excel_env_end, sep="")
+  knitr::asis_output(excel_code)
+})
 
 
 options(OutDec = ",", digits = 3)
@@ -16,11 +36,11 @@ print_func <- function(x) {
   paste0('`',x,'()`')
 }
 
-for(x in list.files("peatykid", "*.Rmd", full.names = T))
-{
-  sisu <- readLines(x)
-  cat(sisu[8:length(sisu)], file = basename(x), sep="\n")
-}
+# for(x in list.files("peatykid", "*.Rmd", full.names = T))
+# {
+#   sisu <- readLines(x)
+#   cat(sisu[8:length(sisu)], file = basename(x), sep="\n")
+# }
 
 for(x in list.files("peatykid/pildid", "*.png", full.names = T))
 {
@@ -29,4 +49,4 @@ for(x in list.files("peatykid/pildid", "*.png", full.names = T))
 
 
 #file.exists("peatykid/data/naited.xlsx")
-file.copy("peatykid/data/naited.xlsx", "data/naited.xlsx", overwrite = T)
+#file.copy("peatykid/data/naited.xlsx", "data/naited.xlsx", overwrite = T)
